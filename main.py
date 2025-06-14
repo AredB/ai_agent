64ebd08e2c99b3704eb45f9ef7e2f31afcaa7b85
AredB <aredbruutal@gmail.com>
2025-06-14 12:41:00 +0200

    AI Agent finish

--- main.py
+++ main.py
@@ -1,19 +1,42 @@
 import os
+import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
+
 
 load_dotenv()
 api_key = os.environ.get("GEMINI_API_KEY")
 
+if len(sys.argv) < 2:
+    print("Error: No prompt provided.")
+    sys.exit(1)
+
+verbose = False
+args = sys.argv[1:]
+if "--verbose" in args:
+    verbose = True
+    args.remove("--verbose")
+
+prompt = " ".join(sys.argv[1:])
+
+messages = [
+    types.Content(role="user", parts=[types.Part(text=prompt)]),
+]
+
 client = genai.Client(api_key=api_key)
 
 response = client.models.generate_content(
-    model='gemini-2.0-flash-001', contents='Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum.'
+    model="gemini-2.0-flash-001",
+    contents=messages,
 )
 
-prompt_tokens = response.usage_metadata.prompt_token_count
-response_tokens = response.usage_metadata.candidates_token_count
+prompt_tokens = getattr(response.usage_metadata, "prompt_token_count", "N/A")
+response_tokens = getattr(response.usage_metadata, "candidates_token_count", "N/A")
 
 print("Generated Response:", response.text)
-print(f"Prompt tokens: {prompt_tokens}")
-print(f"Response tokens: {response_tokens}")
+
+if verbose:
+    print(f'User prompt: "{prompt}"')
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: {response_tokens}")
